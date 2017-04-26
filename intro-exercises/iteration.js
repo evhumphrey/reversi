d834cfdb480ed0983e76d4a247a1bac37fec4df3
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-26 12:13:35 -0700

    Complete String#substrings

--- intro-exercises/iteration.js
+++ intro-exercises/iteration.js
@@ -23,3 +23,20 @@ let a = [6, 5, 3, 2, 3, 7];
 console.log(a);
 a.bubbleSort();
 console.log(a);
+
+String.prototype.substrings = function substrings() {
+  let resultArr = [];
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      let subStr = this.slice(i, j);
+      if (!resultArr.includes(subStr)) {
+        resultArr.push(subStr);
+      }
+    }
+  }
+  return resultArr;
+};
+
+console.log('--substrings--');
+let str = 'hello';
+console.log(str.substrings());
