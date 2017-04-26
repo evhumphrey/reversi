70eb2fe0fcf6de6dc9e2993ece6b57c519a0925b
Elliot Humphrey <elliot.v.humphrey@gmail.com>
2017-04-26 16:50:07 -0700

    Refactor Courses to track scheduling conflicts

--- intro-exercises/students.js
+++ intro-exercises/students.js
@@ -27,13 +27,23 @@ Student.prototype.courseLoad = function courseLoad() {
   return totalLoad;
 };
 
-function Course(name, department, numCredits) {
+function Course(name, department, numCredits, days, blockNum) {
   this.name = name;
   this.department = department;
   this.numCredits = numCredits;
+  this.days = days;
+  this.blockNum = blockNum;
   this.students = [];
 }
 
 Course.prototype.addStudent = function addStudent(student) {
   student.enroll(this);
+};
+
+Course.prototype.conflictsWith = function conflictsWith(otherCourse) {
+  let dayConflicts = this.days.filter((day) => {
+    return otherCourse.days.includes(day);
+  });
+  return this.blockNum === otherCourse.blockNum &&
+         dayConflicts.length !== 0;
 };
